69aa6abff12b73031e924804587f51be4b818273
JUHYUN PARK <jpark440@my.bcit.ca>
2024-02-20 22:36:13 -0800

    Add login with Firebase

---
+++ scripts/skeleton.js
@@ -0,0 +1,13 @@
+// function loadSkeleton() {
+
+//     firebase.auth().onAuthStateChanged(function (user) {
+//         if (user) {                 
+//             console.log($('').load(''));
+//             console.log($('').load(''));
+//         } else {
+//             console.log($('').load(''));
+//             console.log($('').load(''));
+//         }
+//     });
+// }
+// loadSkeleton();
