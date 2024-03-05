af30894b784af4a6db5766a99ea987fb7e9fbdd3
Rahul Sharma <rsharma269@my.bcit.com>
2024-03-05 00:42:54 -0800

    Edit Main Page, script.js and style.css

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,27 @@
-function sayHello() {
-    
+
+function updateDateTime() {
+    // create a new `Date` object
+    const now = new Date();
+
+    // get the current date and time as a string
+    const currentDateTime = now.toLocaleString();
+
+    // update the `textContent` property of the `span` element with the `id` of `dateSpan`
+    document.querySelector('#dateSpan').textContent = currentDateTime.substring(0,8);
+
 }
-//sayHello();
+setInterval(updateDateTime, 1000);
+
+// Motivaitional Quote API
+// const api_url ="https://api.quotable.io/quotes/random";
+
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var quotes = await response.json();
+  let quote = quotes[0].content;
+  document.getElementById("motivQuote").innerHTML = quote;
+  console.log(quote);
+}
+
+getapi(api_url);
