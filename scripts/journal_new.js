457cf3c6d404db5b1a003fe99a81ade2dc62ed21
ROY VINA <roy.jeph@gmail.com>
2024-03-17 20:13:35 -0700

    Add new note functionality

---
+++ scripts/journal_new.js
@@ -0,0 +1,29 @@
+date_creation = document.getElementById("date").innerHTML = new Date().toLocaleString();
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+
+        function addNote() {
+            note = db.collection("users").doc(firebase.auth().currentUser.uid).collection('notes')
+
+            note.add({
+                title: document.getElementById("title").value,
+                date: date_creation,
+                note: document.getElementById("note").value
+            })
+        }
+        
+        try {
+            document.getElementById("save").addEventListener("click", () => {
+                addNote()
+            })
+        } catch(TypeError) {
+            console.log("Save button type error")
+        }
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in.")
+    }
+  });
+
