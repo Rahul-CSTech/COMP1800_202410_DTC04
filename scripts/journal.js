0eba8d6d546c5112788a2042c8a7c63a69169366
ROY VINA <roy.jeph@gmail.com>
2024-03-17 20:15:08 -0700

    Add journal functionality

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,45 +1,29 @@
-async function addNote() {
-    note = await db.collection("journal")
-    note.add({
-        title: document.getElementById("title").value,
-        date: document.getElementById("date").value,
-        note: document.getElementById("note").value,
-    })
-}
-
-function readNote() {
-    let cardTemplate = document.getElementById("note_template")
-
-
-    db.collection("journal").get()
-        .then(notes => {
-            notes.forEach(doc => {
-                var title = doc.data().title
-                var date = doc.data().date
-                var detail = doc.data().note
-                let newcard = document.importNode(cardTemplate.content, true)
-                
-                newcard.getElementById("display_bg").classList.add("bg-submissive")
-                newcard.getElementById("display_title").innerHTML = title
-                newcard.getElementById("display_date").innerHTML = date
-                newcard.getElementById("display_note").innerHTML = detail
-
-                document.getElementById("note_card").appendChild(newcard)
-            })
-        })
-}
-
-try {
-    readNote()
-} catch(ReferenceError) {
-    console.log("Read note reference error.")
-}
-    
-
-try {
-    document.getElementById("save").addEventListener("click", () => {
-        addNote()
-    })
-} catch(TypeError) {
-    console.log("Save button type error")
-}
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        function readNote() {
+            let cardTemplate = document.getElementById("note_template")
+        
+            db.collection("users").doc(user.uid).collection("notes").get()
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        var title = doc.data().title
+                        var date = doc.data().date
+                        var detail = doc.data().note
+                        let newcard = document.importNode(cardTemplate.content, true)
+                        newcard.getElementById("display_bg").classList.add("bg-submissive")
+                        newcard.getElementById("display_title").innerHTML = title
+                        newcard.getElementById("display_date").innerHTML = date
+                        newcard.getElementById("display_note").innerHTML = detail
+                        newcard.querySelector('a').href = "journal_edit.html?docID="+doc.id
+        
+                        document.getElementById("note_card").appendChild(newcard)
+                    })
+                })
+        }
+        readNote();
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in.")
+    }
+});
