cad703cdac1615ca7eb88c4eff0c6a52c02e0def
Rahul Sharma <rsharma269@my.bcit.com>
2024-03-19 11:04:57 -0700

    Make some changes to tasks page and its features

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -3,18 +3,19 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         // User is signed in.
         function makeCard() {
-         
+           
+            
             db.collection("users").doc(user.uid).collection("tasks").get()
                 .then((querySnapshot) => {
                     querySnapshot.forEach((doc) => {
+                    if (doc)   {
+                    var newcard= document.getElementById("taskToday").cloneNode(true)
+                    
                     var taskTime = doc.data().taskTime
                     var taskDate = doc.data().taskDate
                     var taskInfo = doc.data().taskInfo
                     var taskName = doc.data().taskName
-
-                    var newcard= document.getElementById("taskToday").cloneNode(true)
-                    const thislist = document.getElementById("taskToday")
-                    thislist.remove()
+                    
                     newcard.id = taskName
                     newcard.querySelector("#taskTodayName").innerHTML = taskName;
                     newcard.querySelector("#taskTodayInfo").innerHTML = taskInfo;
@@ -22,14 +23,21 @@ firebase.auth().onAuthStateChanged(function(user) {
                     newcard.querySelector("#taskTodayTime").innerHTML = taskTime;
                    
                     document.getElementById("todayTaskList").appendChild(newcard);
+                    } 
+                    
+                    // else
+                    // {    
+                    //     console.log("nothing here")
+                    // }
                         
                         // newcard.querySelector('a').href = "journal_edit.html?docID="+doc.id
                         
-                    }
-                    )
+                    })
                 })
         }
         makeCard();
+        // var thislist = document.getElementById("taskToday")
+        //             thislist.style.visibility ="hidden";
     } else {
         // No user is signed in.
         console.log("No user is signed in.")
