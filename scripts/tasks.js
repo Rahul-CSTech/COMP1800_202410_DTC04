393b145d28a46d881b5d35a801ad26e7d81255d1
Rahul Sharma <rsharma269@my.bcit.com>
2024-03-20 00:22:30 -0700

    Fix tasks card dissapearing feature

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,45 +1,49 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  
+firebase.auth().onAuthStateChanged(function (user) {
+
     if (user) {
+       
         // User is signed in.
         function makeCard() {
            
-            
             db.collection("users").doc(user.uid).collection("tasks").get()
                 .then((querySnapshot) => {
-                    querySnapshot.forEach((doc) => {
-                    if (doc)   {
-                    var newcard= document.getElementById("taskToday").cloneNode(true)
-                    
-                    var taskTime = doc.data().taskTime
-                    var taskDate = doc.data().taskDate
-                    var taskInfo = doc.data().taskInfo
-                    var taskName = doc.data().taskName
-                    
-                    newcard.id = taskName
-                    newcard.querySelector("#taskTodayName").innerHTML = taskName;
-                    newcard.querySelector("#taskTodayInfo").innerHTML = taskInfo;
-                    newcard.querySelector("#taskTodayDate").innerHTML = taskDate; 
-                    newcard.querySelector("#taskTodayTime").innerHTML = taskTime;
-                   
-                    document.getElementById("todayTaskList").appendChild(newcard);
-                    } 
-                    
-                    // else
-                    // {    
-                    //     console.log("nothing here")
-                    // }
-                        
-                        // newcard.querySelector('a').href = "journal_edit.html?docID="+doc.id
-                        
-                    })
-                })
+
+                    if (querySnapshot.empty != true) {
+                        let cardprototype = document.getElementById("taskToday").cloneNode(true);
+                        document.getElementById("taskToday").remove();
+                        console.log(querySnapshot)
+                        console.log(querySnapshot.size)
+
+                        querySnapshot.forEach((doc) => {
+
+                            var newcard = cardprototype.cloneNode(true);
+                            
+                            var taskTime = doc.data().taskTime
+                            var taskDate = doc.data().taskDate
+                            var taskInfo = doc.data().taskInfo
+                            var taskName = doc.data().taskName
+                            newcard.id = taskDate
+
+                            newcard.querySelector("#taskTodayName").innerHTML = taskName;
+                            newcard.querySelector("#taskTodayInfo").innerHTML = taskInfo;
+                            newcard.querySelector("#taskTodayDate").innerHTML = taskDate;
+                            newcard.querySelector("#taskTodayTime").innerHTML = taskTime;
+                            document.getElementById("todayTaskList").appendChild(newcard);
+                        });
+                        // document.getElementById("taskToday").remove();
+                    }
+
+                }
+            )
         }
+        
         makeCard();
-        // var thislist = document.getElementById("taskToday")
-        //             thislist.style.visibility ="hidden";
-    } else {
+        // document.getElementById("todayTaskList").appendChild(cardprototype);
+    }
+
+    else {
         // No user is signed in.
         console.log("No user is signed in.")
     }
 });
+
