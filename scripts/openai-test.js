b4dc6f42ef6d14ebee5d098211daa9dcc8b2db5e
JUHYUN PARK <jpark440@my.bcit.ca>
2024-03-24 16:16:25 -0700

    Add the function

--- scripts/openai-test.js
+++ scripts/openai-test.js
@@ -1,14 +1,22 @@
-import OpenAI from "openai";
+const OpenAI = require('openai');
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  apiKey: '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', // This is the default and can be omitted
+});
+
+// temporary
+const user_messages = [
+  { role: "system", content: "A canadian living in Vancouver" },
+  { role: "user", content: "Tourist spots to recommend for foreigners, only names" },
+]; 
 
 async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
+  const chatCompletion = await openai.chat.completions.create({
+    messages: user_messages,
+    model: 'gpt-3.5-turbo',
+    max_tokens: 100,
   });
-
-  console.log(completion.choices[0]);
+  console.log(chatCompletion.choices[0].message.content);
 }
 
 main();
