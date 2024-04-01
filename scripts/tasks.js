f743f1fe73c1a1878ff3ba0a16b19ea88147c0a0
Rahul Sharma <rsharma269@my.bcit.com>
2024-03-31 18:40:36 -0700

    Sync changes

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -29,12 +29,10 @@ firebase.auth().onAuthStateChanged(function (user) {
                             var row = addDelRow.cloneNode(true);
                             newcard.appendChild(row);
                             newcard.addEventListener("click", () => {row.classList.toggle("hidden")
-                                newcard.classList.toggle("pb-0")
-                        });
+                                 newcard.classList.toggle("pb-0")
+                                });
+                            newcard.querySelector("#deleteButton").addEventListener("click", () => {newcard.remove()})
                            
-                           
-                           
-
                             newcard.querySelector("#taskTodayName").innerHTML = taskName;
                             newcard.querySelector("#taskTodayInfo").innerHTML = taskInfo;
                             newcard.querySelector("#taskTodayDate").innerHTML = taskDate;
